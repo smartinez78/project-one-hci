cddcd1e747ed6cbd527eea0040a8ea7d8eaa7079
Sofia Martinez <smartinez@hawk.iit.edu>
2019-02-10 15:54:56 -0600

    Add event listener to listen for telephone number input; enable submit button when good number is entered

--- site.js
+++ site.js
@@ -12,12 +12,19 @@
   // disable submit button
   form_submit.setAttribute('disabled','disabled');
 
+  // select elements from DOM content and store in variables
   var mailing_list = document.querySelector('#mailing-list')
-  var form_submit = document.querySelector('#submit');
   var email_input = document.querySelector('#email');
   var phone_input = document.querySelector('#telephone');
 
+  // listen for keyup anywhere on form
+  mailing_list.addEventListener('keyup',function(){
+   var clean_number = phone_input.value.replace(/\D/g,'');
+   var final_number = clean_number.replace(/^1/,'');
+   // enable submit button if final number is acceptable
+   if (sanitized_number.length === 10) {
+     form_submit.removeAttribute('disabled');
+   }
+  }); // end of mailing_list event listener
   }); // end of DOM content
-
-
 }());  // end of function
