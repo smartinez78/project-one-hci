bbdf5c1f819d7930a49c4e154d90d31db847032b
Sofia Martinez <smartinez@hawk.iit.edu>
2019-02-10 15:27:52 -0600

    Add javascript to select elements from the DOM

--- site.js
+++ site.js
@@ -9,7 +9,15 @@
 
   // check to see if DOM content has loaded
   document.addEventListener('DOMContentLoaded',function(){
-    // disable submit button
-    form_submit.setAttribute('disabled','disabled');
-  });
-}());
+  // disable submit button
+  form_submit.setAttribute('disabled','disabled');
+
+  var mailing_list = document.querySelector('#mailing-list')
+  var form_submit = document.querySelector('#submit');
+  var email_input = document.querySelector('#email');
+  var phone_input = document.querySelector('#telephone');
+
+  }); // end of DOM content
+
+
+}());  // end of function
