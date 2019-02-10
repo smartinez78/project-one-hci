563dc49ff50215442e2db8c823d1f989a0e72870
Sofia Martinez <smartinez@hawk.iit.edu>
2019-02-10 16:14:56 -0600

    Fix errors caught by linter

--- site.js
+++ site.js
@@ -1,30 +1,31 @@
+"use strict";
+
 (function(){
+  // select elements from DOM content and store in variables
+  var form_submit = document.querySelector('#submit');
+  var mailing_list = document.querySelector('#mailing-list');
+  // var email_input = document.querySelector('#email');
+  var phone_input = document.querySelector('#telephone');
+
   // browser check
   if (!('querySelector' in document && 'addEventListener' in document)) {
     console.log('Old browser');
     return;
   }
 
-  var form_submit = document.querySelector('#submit');
-
   // check to see if DOM content has loaded
-  document.addEventListener('DOMContentLoaded',function(){
-  // disable submit button
-  form_submit.setAttribute('disabled','disabled');
+  document.addEventListener('DOMContentLoaded', function(){
+    // disable submit button
+    form_submit.setAttribute('disabled', 'disabled');
 
-  // select elements from DOM content and store in variables
-  var mailing_list = document.querySelector('#mailing-list')
-  var email_input = document.querySelector('#email');
-  var phone_input = document.querySelector('#telephone');
-
-  // listen for keyup anywhere on form
-  mailing_list.addEventListener('keyup',function(){
-   var clean_number = phone_input.value.replace(/\D/g,'');
-   var final_number = clean_number.replace(/^1/,'');
-   // enable submit button if final number is acceptable
-   if (sanitized_number.length === 10) {
-     form_submit.removeAttribute('disabled');
-   }
-  }); // end of mailing_list event listener
+    // listen for keyup anywhere on form
+    mailing_list.addEventListener('keyup', function(){
+      var clean_number = phone_input.value.replace(/\D/g, '');
+      var final_number = clean_number.replace(/^1/, '');
+      // enable submit button if final number is acceptable
+      if (final_number.length === 10) {
+        form_submit.removeAttribute('disabled');
+      }
+    }); // end of mailing_list event listener
   }); // end of DOM content
 }());  // end of function
